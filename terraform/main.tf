ad6cdedb75198421a1f1241d3e92ca1cbc8ca153
Oscar Ramos <325296232+oscar-ramos-ezequiel@users.noreply.github.com>
2026-09-09 19:33:52 +0200

    feat: define existing Azure infrastructure as Terraform resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,34 @@
-data "azurerm_resource_group" "portfolio" {
-  name = var.resource_group_name
+resource "azurerm_resource_group" "portfolio" {
+  name     = var.resource_group_name
+  location = "West Europe"
+
+  tags = {
+    Environment = "Development"
+    Project     = "Azure-Cloud-Portfolio"
+    ManagedBy   = "Terraform"
+    Purpose     = "Learning"
+  }
 }
 
-data "azurerm_linux_web_app" "portfolio" {
+resource "azurerm_service_plan" "portfolio" {
+  name                = "ASP-rgportfoliowebappdev001-afe5"
+  resource_group_name = azurerm_resource_group.portfolio.name
+  location            = "France Central"
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "portfolio" {
   name                = var.app_service_name
-  resource_group_name = data.azurerm_resource_group.portfolio.name
+  resource_group_name = azurerm_resource_group.portfolio.name
+  location            = azurerm_service_plan.portfolio.location
+  service_plan_id     = azurerm_service_plan.portfolio.id
+
+  https_only = true
+
+  site_config {
+    application_stack {
+      python_version = "3.14"
+    }
+  }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
 output "resource_group_name" {
-  description = "Azure Resource Group managed by the project"
-  value       = data.azurerm_resource_group.portfolio.name
+  description = "Azure Resource Group managed by Terraform"
+  value       = azurerm_resource_group.portfolio.name
 }
 
 output "resource_group_location" {
   description = "Azure Resource Group location"
-  value       = data.azurerm_resource_group.portfolio.location
+  value       = azurerm_resource_group.portfolio.location
 }
 
 output "app_service_name" {
   description = "Azure App Service name"
-  value       = data.azurerm_linux_web_app.portfolio.name
+  value       = azurerm_linux_web_app.portfolio.name
 }
 
 output "app_service_default_hostname" {
   description = "Default hostname of the Azure App Service"
-  value       = data.azurerm_linux_web_app.portfolio.default_hostname
+  value       = azurerm_linux_web_app.portfolio.default_hostname
 }
